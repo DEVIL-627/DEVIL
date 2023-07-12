2366eaa6554741e5efb9b32fc2c94f6c4b1c20a9
MaNi X KaZaMa <138804887+DEVIL-627@users.noreply.github.com>
2023-07-12 15:09:46 +0500

    Update DEVIL.py

--- DEVIL.py
+++ DEVIL.py
@@ -4,6 +4,5 @@ os.system('git pull')
 DEVIL =platform.architecture()[0]
 if DEVIL =="32bit":
     __import__("D3VIL32")
-elif DEVIL=="64bit":
-     __import__("D3VIL")
+elif DEVIL=="64bit":__import__("D3VIL")
  
